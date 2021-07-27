9324d4a38524984ab8cb6e764f8671ff02fd2509
bluewow <dogseen@gmail.com>
2021-07-27 19:25:16 +0900

    github action test

--- src/main/java/com/example/study/controller/HelloController.java
+++ src/main/java/com/example/study/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello() {
-        return "hello spring";
+        return "hello spring11";
     }
 
     @GetMapping("pr")
