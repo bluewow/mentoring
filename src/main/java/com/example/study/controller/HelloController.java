4d72d22493624cc60f8bc1a93cfd510bffeb0368
bluewow <dogseen@gmail.com>
2021-08-03 08:46:20 +0900

    update swagger

--- src/main/java/com/example/study/controller/HelloController.java
+++ src/main/java/com/example/study/controller/HelloController.java
@@ -1,16 +1,21 @@
 package com.example.study.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = {"헬로우 컨트롤러"})
 @RestController
 public class HelloController {
 
+    @ApiOperation("Hello")
     @GetMapping("hello")
     public String hello() {
-        return "hello spring11";
+        return "hello spring1111";
     }
 
+    @ApiOperation("PR TEST")
     @GetMapping("pr")
     public String prTest() {
         return "pr test";
--- src/main/java/com/example/study/controller/MemberController.java
+++ src/main/java/com/example/study/controller/MemberController.java
@@ -3,11 +3,14 @@ package com.example.study.controller;
 import com.example.study.controller.dto.JoinMember;
 import com.example.study.controller.dto.ResponseMember;
 import com.example.study.service.MemberService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"Member 컨트롤러"})
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
